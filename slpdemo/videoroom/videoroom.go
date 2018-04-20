d5d73fc1a0e611d2e4f3c92854e08134ef11bbc8
Cibifang <cifang_bifang@hotmail.com>
2018-04-20 16:59:15 +0800

    add new slp "videoroom"
    This module will translate jsip message to Janus Server and
    implement an audio/video router

---
+++ slpdemo/videoroom/videoroom.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+    "fmt"
+    "container/heap"
+    "time"
+
+    "github.com/go-ini/ini"
+    "rtclib"
+    "janus"
+)
+
+
+type Config struct {
+    janusAddr       string
+    maxConcurrent   int
+}
+
+type session struct {
+    jsipID        string
+    janusConn    *janus.Janus
+    id            int
+}
+
+type Videoroom struct {
+    task       *rtclib.Task
+    config     *Config
+    sessions    map[string](*session)
+    rooms       map[string]int
+    jh         *janusHeap
+}
+
+func GetInstance(task *rtclib.Task) rtclib.SLP {
+    var vr = &Videoroom{task: task,
+                        sessions: make(map[string](*session)),
+                        jh: &janusHeap{}}
+    if !vr.loadConfig() {
+        fmt.Println("Videoroom load config failed")
+    }
+
+    heap.Init(vr.jh)
+
+    return vr
+}
+
+func (vr *Videoroom) loadConfig() bool {
+    vr.config = new(Config)
+
+    confPath := rtclib.RTCPATH + "/conf/Videoroom.ini"
+
+    f, err := ini.Load(confPath)
+    if err != nil {
+        fmt.Printf("Load config file %s error: %v", confPath, err)
+        return false
+    }
+
+    return rtclib.Config(f, "Videoroom", vr.config)
+}
+
+func (vr *Videoroom) cachedOrNewJanus() *janus.Janus {
+    if vr.jh.Len() > 0 && (*vr.jh)[0].numSess < vr.config.maxConcurrent {
+        (*vr.jh)[0].numSess += 1
+        return (*vr.jh)[0].janusConn
+    }
+
+    j := janus.NewJanus(vr.config.janusAddr)
+    go j.WaitMsg()
+
+    wait := 0
+    for j.ConnectStatus() == false {
+        if wait >= 5 {
+            fmt.Printf("wait %d s to connect, quit", wait)
+            return nil
+        }
+        timer := time.NewTimer(time.Second * 1)
+        <- timer.C
+        wait += 1
+        fmt.Printf("wait %d s to connect", wait)
+    }
+
+    heap.Push(vr.jh, &janusItem{janusConn: j, numSess: 1})
+
+    return j
+}
+
+func (vr *Videoroom) session(DialogueID string) (*session, bool) {
+    sess, exist := vr.sessions[DialogueID]
+    if exist {
+        return sess, true
+    }
+
+    conn := vr.cachedOrNewJanus()
+    if conn == nil {
+        return nil, false
+    }
+    vr.sessions[DialogueID] = &session{jsipID: DialogueID,
+                                       janusConn: conn}
+    fmt.Printf("create videoroom for DialogueID %s success", DialogueID)
+    return vr.sessions[DialogueID], true
+}
+
+func (s *session) newSession() {
+    var msg janus.ClientMsg
+    j := s.janusConn
+
+    tid := j.NewTransaction()
+
+    msg.Janus = "create"
+    msg.Transaction = tid
+
+    j.Send(msg)
+    reqChan, ok := j.MsgChan(tid)
+    if !ok {
+        fmt.Printf("create: can't find channel for tid %s", tid)
+    }
+
+    req := <- reqChan
+    fmt.Printf("receive from channel: %+v", req)
+    j.NewSess(req.Data.Id)
+    s.id = req.Data.Id
+
+    fmt.Printf("create janus session %d success", s.id)
+}
+
+
+func (vr *Videoroom) Process(jsip *rtclib.JSIP) int {
+    fmt.Println("recv msg: ", jsip)
+
+    switch jsip.Type {
+    case rtclib.INVITE:
+        sess, ok := vr.session(jsip.DialogueID)
+        if !ok {
+            return rtclib.FINISH
+        }
+        sess.newSession()
+        // vr.newJanusSession()
+        // vr.attach()
+        // vr.getRoom()
+        // vr.joinRoom()
+        // vr.offer()
+        resp := &JSIP{
+            Type:       jsip.Type,
+            Code:       200,
+            From:       jsip.From,
+            To:         jsip.To,
+            CSeq:       jsip.CSeq,
+            DialogueID: jsip.DialogueID,
+            RawMsg:     make(map[string]interface{}),
+            Body:       "create sess " + sess.id +
+        }
+
+        SendJsonSIPMsg(nil, resp)
+    case rtclib.ACK:
+        rtclib.SendJSIPBye(rtclib.Jsessions[jsip.DialogueID])
+    }
+
+    return rtclib.CONTINUE
+}
+
