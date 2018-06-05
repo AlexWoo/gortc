f20d67162647ef8bbe2dcb6c0c04ee333761b9f2
AlexWoo(武杰) <wj19840501@gmail.com>
2018-06-05 18:39:40 +0800

    add timer

---
+++ src/rtclib/timer.go
@@ -0,0 +1,46 @@
+// Copyright (C) AlexWoo(Wu Jie) wj19840501@gmail.com
+//
+// Timer
+
+package rtclib
+
+import (
+	"time"
+)
+
+type Timer struct {
+	timer   *time.Timer
+	quit    chan bool
+	handler func(d interface{})
+	data    interface{}
+}
+
+func NewTimer(d time.Duration, f func(interface{}), p interface{}) *Timer {
+	timer := &Timer{
+		timer:   time.NewTimer(d),
+		quit:    make(chan bool),
+		handler: f,
+		data:    p,
+	}
+
+	go func() {
+		select {
+		case <-timer.timer.C:
+			timer.handler(timer.data)
+		case <-timer.quit:
+			timer.timer.Stop()
+		}
+	}()
+
+	return timer
+}
+
+func (t *Timer) Stop() {
+	if t.timer != nil {
+		t.quit <- true
+	}
+}
+
+func (t *Timer) Reset(d time.Duration) {
+	t.timer.Reset(d)
+}
