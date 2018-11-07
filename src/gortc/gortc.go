b5a75118d59992b26b0e7a4a1f8f7efb3b69b529
AlexWoo(武杰) <wj19840501@gmail.com>
2018-11-07 09:55:19 +0800

    add -h and -d option for gortc help and start background

--- src/gortc/gortc.go
+++ src/gortc/gortc.go
@@ -5,8 +5,11 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"rtclib"
 	"runtime"
+	"syscall"
 
 	"github.com/alexwoo/golib"
 )
@@ -15,12 +18,53 @@ var (
 	rtcpath = "/usr/local/gortc/"
 )
 
+func usage() {
+	fmt.Printf("usage: %s -h\n", os.Args[0])
+	fmt.Printf("usage: %s [-d]\n", os.Args[0])
+	fmt.Printf("    -d    start backgroud\n")
+	os.Exit(1)
+}
+
+func deamon() {
+	if syscall.Getppid() == 1 { // already daemon
+		f, err := os.OpenFile("/dev/null", os.O_RDWR, 0)
+		if err != nil {
+			fmt.Println("open /dev/null failed")
+			os.Exit(-1)
+		}
+
+		fd := f.Fd()
+		syscall.Dup2(int(fd), int(os.Stdin.Fd()))
+		syscall.Dup2(int(fd), int(os.Stdout.Fd()))
+
+		return
+	}
+
+	args := append([]string{os.Args[0]}, os.Args[1:]...)
+	os.StartProcess(os.Args[0], args,
+		&os.ProcAttr{Files: []*os.File{os.Stdin, os.Stdout, os.Stderr}})
+
+	os.Exit(0)
+}
+
 func init() {
 	rtclib.RTCPATH = rtcpath
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
 func main() {
+	opt := golib.NewOptParser()
+	for opt.GetOpt("hd") {
+		switch opt.Opt() {
+		case 'h':
+			usage()
+		case 'd':
+			deamon()
+		case '?':
+			usage()
+		}
+	}
+
 	ms := golib.NewModules()
 
 	ms.AddModule("main", &mainModule{})
